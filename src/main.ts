073d6d829f674bfa007b809801dac1a1f0f54ce1
Sebastien Lehmstedt <lehmstedt-sebastien@hotmail.fr>
2025-04-02 11:41:53 -0400

    Full functional loop with won and lost states

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
+    <h1>Try to stop on the green area</h1>
+    <p>Use arrow up to throttle and arrow down to brake</p>
+    <div>
+      <button id="restart">Retry</button>
+    </div>
+    
+    
     <canvas id="canvas" width="1000" height="1000"></canvas>
   </div>
 `
@@ -11,14 +18,48 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 let throttle: number = 0;
 let stop = false;
 
+const canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement
+const ctx = canvas.getContext("2d");
+
 const updateGame = (ctx: CanvasRenderingContext2D, state: State) => {
   const newState = update(state, throttle)
+
+  if(newState.lost){    
+    ctx.reset()
+    ctx.font = "35px sans-serif";
+    ctx.textAlign = "center"
+    ctx.fillText("You went too far, maybe try to brake early next time ?", 500, 500)
+    return
+  }
+  if(newState.won){    
+    ctx.reset()
+    ctx.font = "35px sans-serif";
+    ctx.textAlign = "center"
+    ctx.fillText("You made it. Want to try to beat you previous score ?", 500, 500)
+    return
+  }
   throttle = 0
   ctx.reset()
+  ctx.fillStyle = "green"
+  ctx.fillRect(0, 0, 1000, 100)  
   ctx.fillStyle = "red"
-  ctx.fillRect(500, 900 - newState.position / 10, 10, 10)
+  ctx.fillRect(500, 1000 - newState.position / 10, 10, 10)
+  
 
   setTimeout(() => updateGame(ctx, newState), 10)
+}
+
+const runANewGame = () => {
+  if(ctx){
+
+    const gameOverParagraph = document.getElementById("game-over")
+    if(gameOverParagraph){
+      gameOverParagraph.innerText = "Go go go !"
+    }
+    const state = new State()
+
+    updateGame(ctx, state);
+  }
 }
 
 
@@ -33,11 +74,14 @@ document.addEventListener("keydown", (event) => {
     stop = true;
   }
 })
-const canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement
-const ctx = canvas.getContext("2d");
-if(ctx){
-    const state = new State()
-    updateGame(ctx, state);
+
+const restartButton = document.getElementById("restart")
+if(restartButton){
+  restartButton.addEventListener("click", () => runANewGame())
 }
+
+runANewGame()
+
+
 
 
